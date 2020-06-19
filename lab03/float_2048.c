fd662d27ee9e7f85f83ef0e1233be7aefcdcd66a
Soorria Saruva <soorria.ss@gmail.com>
2020-06-19 12:00:46 +1000

    Add bits code to 2048

--- lab03/float_2048.c
+++ lab03/float_2048.c
@@ -15,6 +15,46 @@ uint32_t components_to_bits(float_components_t bits) {
     return result;
 }
 
+// separate out the 3 components of a float
+float_components_t float_bits(uint32_t f) {
+    float_components_t result;
+
+    uint32_t sign_mask = (uint32_t) 1 << 31;
+    result.sign = (f & sign_mask) >> 31;
+
+    uint32_t exponent_mask = (uint32_t) 0xFF << 23;
+    result.exponent = (f & exponent_mask) >> 23;
+
+    uint32_t fraction_mask = 0x7FFFFF; // Last 23 bits
+    result.fraction = f & fraction_mask;
+
+    return result;
+}
+
+// given the 3 components of a float
+// return 1 if it is NaN, 0 otherwise
+int is_nan(float_components_t f) {
+    return f.exponent == 0xFF && f.fraction != 0;
+}
+
+// given the 3 components of a float
+// return 1 if it is inf, 0 otherwise
+int is_positive_infinity(float_components_t f) {
+    return f.sign == 0 && f.exponent == 0xFF && f.fraction == 0;
+}
+
+// given the 3 components of a float
+// return 1 if it is -inf, 0 otherwise
+int is_negative_infinity(float_components_t f) {
+    return f.sign == 1 && f.exponent == 0xFF && f.fraction == 0;
+}
+
+// given the 3 components of a float
+// return 1 if it is 0 or -0, 0 otherwise
+int is_zero(float_components_t f) {
+    return f.exponent == 0 && f.fraction == 0;
+}
+
 // float_2048 is given the bits of a float f as a uint32_t
 // it uses bit operations and + to calculate f * 2048
 // and returns the bits of this value as a uint32_t
