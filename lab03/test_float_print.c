441e19ce468393568be26d9cf8f55c63e9f144b9
Soorria Saruva <soorria.ss@gmail.com>
2020-06-19 10:16:47 +1000

    Do exercise 1

--- lab03/test_float_print.c
+++ lab03/test_float_print.c
@@ -1,1 +1,19 @@
-/web/cs1521/20T2/activities/float_print/files.ln/test_float_print.c
+// Print out a float.
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "floats.h"
+
+int main(int argc, char *argv[]) {
+    for (int arg = 1; arg < argc; arg++) {
+        union overlay input;
+
+        input.f = atof(argv[arg]);
+        float_print(input.u);
+    }
+
+    return 0;
+}
