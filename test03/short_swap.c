64272da0ae1128a32e9a87cefa988ad3625ef33b
Soorria Saruva <soorria.ss@gmail.com>
2020-06-22 09:44:01 +1000

    Do short_swap

--- test03/short_swap.c
+++ test03/short_swap.c
@@ -7,6 +7,8 @@
 // given uint16_t value return the value with its bytes swapped
 uint16_t short_swap(uint16_t value) {
     // PUT YOUR CODE HERE
-
-    return 42;
+    uint16_t first_mask = (0xFF << 8);
+    uint16_t first_byte = (value & first_mask) >> 8;
+    uint16_t second_byte = (value & 0xFF);
+    return (second_byte << 8) + first_byte;
 }
