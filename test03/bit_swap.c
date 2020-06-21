233bf706384bd981693ab8bf7fdff0e874f330a0
Soorria Saruva <soorria.ss@gmail.com>
2020-06-22 09:55:14 +1000

    Do bit_swap

--- test03/bit_swap.c
+++ test03/bit_swap.c
@@ -6,7 +6,13 @@
 
 // return value with pairs of bits swapped
 uint64_t bit_swap(uint64_t value) {
-    // PUT YOUR CODE HERE
-
-    return 42;
+    uint64_t result = 0;
+    for (int i = 0; i < 32; i++) {
+        uint64_t bit1_mask = (uint64_t)1 << (2 * i);
+        uint64_t bit1 = (value & bit1_mask) >> (2 * i);
+        uint64_t bit2_mask = (uint64_t)1 << (2 * i + 1);
+        uint64_t bit2 = (value & bit2_mask) >> (2 * i + 1);
+        result += (bit1 << (2 * i + 1)) + (bit2 << (2 * i));
+    }
+    return result;
 }
