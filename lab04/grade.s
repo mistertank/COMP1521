0978fecbd0b5fd6b1ea18bd402ef1b7117056e5e
Soorria Saruva <soorria.ss@gmail.com>
2020-06-26 09:54:09 +1000

    do exercise 2

--- lab04/grade.s
+++ lab04/grade.s
@@ -7,13 +7,61 @@ main:
 
     li $v0, 5           # scanf("%d", mark);
     syscall
+    move $t0, $v0
 
+    li $t1, 50
+    blt $t0, $t1, handle_fl
+
+    li $t1, 65
+    blt $t0, $t1, handle_ps
+
+    li $t1, 75
+    blt $t0, $t1, handle_cr
+
+    li $t1, 85
+    blt $t0, $t1, handle_dn
+
+    b handle_hd
+    b end
+
+
+handle_fl:
     la $a0, fl          # printf("FL\n");
     li $v0, 4
     syscall
 
+    b end
 
-    jr $ra              # return
+handle_ps:
+    la $a0, ps
+    li $v0, 4
+    syscall
+
+    b end
+
+handle_cr:
+    la $a0, cr
+    li $v0, 4
+    syscall
+
+    b end
+
+handle_dn:
+    la $a0, dn
+    li $v0, 4
+    syscall
+
+    b end
+
+handle_hd:
+    la $a0, hd
+    li $v0, 4
+    syscall
+
+    b end
+
+end:
+    jr $ra
 
     .data
 prompt:
