5668308dd0c32d9026a5f97fe8d563d62c6caf16
Soorria Saruva <soorria.ss@gmail.com>
2020-06-26 10:44:32 +1000

    do count.s

--- lab04/count.s
+++ lab04/count.s
@@ -7,17 +7,29 @@ main:                           # int main(void)
 
     li  $v0, 5                  # scanf("%d", number);
     syscall
+    move $s0, $v0
 
-    li  $a0, 42                 # printf("%d", 42);
+    li  $t0, 1
+    b loop
+
+loop:
+    bgt $t0, $s0, end   # while (t0 <= s0)
+
+    move $a0, $t0       # print(t0)
     li  $v0, 1
     syscall
 
-    li   $a0, '\n'      # printf("%c", '\n');
+    li $a0, 0xa
+    # li   $a0, '\n'
     li   $v0, 11
     syscall
 
+    addi $t0, $t0, 1
+
+    b loop
+
 end:
-    jr  $ra                     # return
+    jr  $ra             # return
 
     .data
 prompt:
