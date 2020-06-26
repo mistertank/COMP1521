c93536e0a1b30b5f7ee365aef703cc9c49b6c214
Soorria Saruva <soorria.ss@gmail.com>
2020-06-26 13:02:36 +1000

    complete bit_rotate.c

--- test04/bit_rotate.c
+++ test04/bit_rotate.c
@@ -2,5 +2,13 @@
 
 // return the value bits rotated left n_rotations
 uint16_t bit_rotate(int n_rotations, uint16_t bits) {
-    return 42; //REPLACE ME WITH YOUR CODE
+    if (n_rotations >= 0) {
+        n_rotations %= 16;
+        return (bits << n_rotations) + (bits >> n_rotations);
+    }
+
+    n_rotations = (-n_rotations) % 16;
+    uint16_t mask = 0xFFFFu >> (16 - n_rotations);
+    uint16_t first_n_bits = (bits & mask) << (16 - n_rotations);
+    return (bits >> n_rotations) + first_n_bits;
 }
