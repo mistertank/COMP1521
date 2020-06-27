9c07a96692c83bdd6d05fcb3418ba704b2ee0dd7
Soorria Saruva <soorria.ss@gmail.com>
2020-06-27 19:57:45 +1000

    Add assignment1 files

---
+++ assignment1/cellular.s
@@ -0,0 +1,113 @@
+########################################################################
+# COMP1521 20T2 --- assignment 1: a cellular automaton renderer
+#
+# Written by <<YOU>>, July 2020.
+
+
+# Maximum and minimum values for the 3 parameters.
+
+MIN_WORLD_SIZE	=    1
+MAX_WORLD_SIZE	=  128
+MIN_GENERATIONS	= -256
+MAX_GENERATIONS	=  256
+MIN_RULE	=    0
+MAX_RULE	=  255
+
+# Characters used to print alive/dead cells.
+
+ALIVE_CHAR	= '#'
+DEAD_CHAR	= '.'
+
+# Maximum number of bytes needs to store all generations of cells.
+
+MAX_CELLS_BYTES	= (MAX_GENERATIONS + 1) * MAX_WORLD_SIZE
+
+	.data
+
+# `cells' is used to store successive generations.  Each byte will be 1
+# if the cell is alive in that generation, and 0 otherwise.
+
+cells:	.space MAX_CELLS_BYTES
+
+
+# Some strings you'll need to use:
+
+prompt_world_size:	.asciiz "Enter world size: "
+error_world_size:	.asciiz "Invalid world size\n"
+prompt_rule:		.asciiz "Enter rule: "
+error_rule:		.asciiz "Invalid rule\n"
+prompt_n_generations:	.asciiz "Enter how many generations: "
+error_n_generations:	.asciiz "Invalid number of generations\n"
+
+	.text
+
+	#
+	# REPLACE THIS COMMENT WITH A LIST OF THE REGISTERS USED IN
+	# `main', AND THE PURPOSES THEY ARE ARE USED FOR
+	#
+	# YOU SHOULD ALSO NOTE WHICH REGISTERS DO NOT HAVE THEIR
+	# ORIGINAL VALUE WHEN `run_generation' FINISHES
+	#
+
+main:
+	#
+	# REPLACE THIS COMMENT WITH YOUR CODE FOR `main'.
+	#
+
+
+	# replace the syscall below with
+	#
+	# li	$v0, 0
+	# jr	$ra
+	#
+	# if your code for `main' preserves $ra by saving it on the
+	# stack, and restoring it after calling `print_world' and
+	# `run_generation'.  [ there are style marks for this ]
+
+	li	$v0, 10
+	syscall
+
+
+
+	#
+	# Given `world_size', `which_generation', and `rule', calculate
+	# a new generation according to `rule' and store it in `cells'.
+	#
+
+	#
+	# REPLACE THIS COMMENT WITH A LIST OF THE REGISTERS USED IN
+	# `run_generation', AND THE PURPOSES THEY ARE ARE USED FOR
+	#
+	# YOU SHOULD ALSO NOTE WHICH REGISTERS DO NOT HAVE THEIR
+	# ORIGINAL VALUE WHEN `run_generation' FINISHES
+	#
+
+run_generation:
+
+	#
+	# REPLACE THIS COMMENT WITH YOUR CODE FOR `run_generation'.
+	#
+
+	jr	$ra
+
+
+	#
+	# Given `world_size', and `which_generation', print out the
+	# specified generation.
+	#
+
+	#
+	# REPLACE THIS COMMENT WITH A LIST OF THE REGISTERS USED IN
+	# `print_generation', AND THE PURPOSES THEY ARE ARE USED FOR
+	#
+	# YOU SHOULD ALSO NOTE WHICH REGISTERS DO NOT HAVE THEIR
+	# ORIGINAL VALUE WHEN `print_generation' FINISHES
+	#
+
+print_generation:
+
+	#
+	# REPLACE THIS COMMENT WITH YOUR CODE FOR `print_generation'.
+	#
+
+	jr	$ra
