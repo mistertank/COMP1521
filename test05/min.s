39b7d5cb5621c65f194d91b5abe7aa80fdf13c44
Soorria Saruva <soorria.ss@gmail.com>
2020-07-03 16:11:47 +1000

    Do min.s

--- test05/min.s
+++ test05/min.s
@@ -8,10 +8,20 @@ main:
     syscall             #
     move $t1, $v0
 
+    bge $t0, $t1, print_y
 
-    li $a0, 42          #   printf("%d\n", 42);
+print_x:
+    move $a0, $t0
+    b   do_print
+
+print_y:
+    move $a0, $t1
+    b   do_print
+
+do_print:
     li $v0, 1
     syscall
+
     li   $a0, '\n'      #   printf("%c", '\n');
     li   $v0, 11
     syscall
