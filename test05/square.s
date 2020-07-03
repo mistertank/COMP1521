6d579ec14495f2f0a32ba81b8e1b1e72283fffa5
Soorria Saruva <soorria.ss@gmail.com>
2020-07-03 16:19:34 +1000

    do square

--- test05/square.s
+++ test05/square.s
@@ -3,12 +3,33 @@ main:
     syscall             #
     move $t0, $v0
 
-    li $a0, 42          #   printf("%d\n", 42);
-    li $v0, 1
+    li $t1, 0
+outer_loop:
+    bge $t1, $t0, outer_end
+
+    li $t2, 0
+
+inner_loop:
+    bge $t2, $t0, inner_end
+
+    li $a0, '*'
+    li $v0, 11
     syscall
+
+    addi $t2, $t2, 1
+    b inner_loop
+
+inner_end:
+
+    addi $t1, $t1, 1
+
     li   $a0, '\n'      #   printf("%c", '\n');
     li   $v0, 11
     syscall
+
+    b outer_loop
+
+outer_end:
 
 end:
 
