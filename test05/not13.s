475aece47844e54752a8068b321479ecd3290223
Soorria Saruva <soorria.ss@gmail.com>
2020-07-03 16:15:42 +1000

    do not13

--- test05/not13.s
+++ test05/not13.s
@@ -7,13 +7,26 @@ main:
     syscall             #
     move $t1, $v0
 
+    move $t2, $t0
+    addi $t2, $t2, 1
 
-    li $a0, 42          #   printf("%d\n", 42);
+loop0:
+    bge $t2, $t1, end0
+
+    beq $t2, 13, skip_print
+
+    move $a0, $t2
     li $v0, 1
     syscall
     li   $a0, '\n'      #   printf("%c", '\n');
     li   $v0, 11
     syscall
+
+skip_print:
+    addi $t2, $t2, 1
+    b loop0
+
+end0:
 
 end:
 
