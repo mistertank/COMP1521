553659d3fc1b41e2adfc8c46999a933877901380
Soorria Saruva <soorria.ss@gmail.com>
2020-07-12 15:23:47 +1000

    Add weekly test 06 files

---
+++ test06/not_negative.s
@@ -0,0 +1,28 @@
+#  read numbers until a non-negative number entered
+# x in $t0
+main:
+    la $a0, str0         # printf("Enter a number: ");
+    li $v0, 4
+    syscall
+
+    li $v0, 5            # scanf("%d", &x);
+    syscall              #
+    move $t0, $v0
+
+    la $a0, str1         # printf("You entered: ");
+    li $v0, 4
+    syscall
+    move $a0, $t0        # printf("%d", x);
+    li $v0, 1
+    syscall
+    li   $a0, '\n'       # printf("%c", '\n');
+    li   $v0, 11
+    syscall
+    li $v0, 0            # return 0
+    jr $31
+
+.data
+str0:
+    .asciiz "Enter a number: "
+str1:
+    .asciiz "You entered: "
