e7aa21c1d3043dcae343a34e2cb81256f5ec2cae
Soorria Saruva <soorria.ss@gmail.com>
2020-07-17 05:09:49 +1000

    Add lab07 files

---
+++ lab07/sieve.s
@@ -0,0 +1,12 @@
+# Sieve of Eratosthenes
+# https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+main:
+
+    # PUT YOUR CODE
+
+    li $v0, 0           # return 0
+    jr $31
+
+.data
+prime:
+    .space 1000
