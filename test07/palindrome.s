d4487d59aac69577cef2fc04e9102fdbd796b3f6
Soorria Saruva <soorria.ss@gmail.com>
2020-07-17 18:55:49 +1000

    Add test07 files

---
+++ test07/palindrome.s
@@ -0,0 +1,38 @@
+# read a line and print whether it is a palindrom
+
+main:
+    la   $a0, str0       # printf("Enter a line of input: ");
+    li   $v0, 4
+    syscall
+
+    la   $a0, line
+    la   $a1, 256
+    li   $v0, 8          # fgets(buffer, 256, stdin)
+    syscall              #
+
+
+    la   $a0, not_palindrome
+    li   $v0, 4
+    syscall
+
+    la   $a0, palindrome
+    li   $v0, 4
+    syscall
+
+    li   $v0, 0          # return 0
+    jr   $31
+
+
+.data
+str0:
+    .asciiz "Enter a line of input: "
+palindrome:
+    .asciiz "palindrome\n"
+not_palindrome:
+    .asciiz "not palindrome\n"
+
+
+# line of input stored here
+line:
+    .space 256
+
