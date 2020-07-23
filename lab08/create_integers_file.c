190a5d986ac016e73c127080b27509989eb5ff3a
Soorria Saruva <soorria.ss@gmail.com>
2020-07-24 09:38:34 +1000

    Add gitignore and add main functions

---
+++ lab08/create_integers_file.c
@@ -0,0 +1,25 @@
+// create_integers_file.c
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 4) {
+        fprintf(stderr, "usage: %s FILENAME MIN MAX\n", argv[0]);
+    }
+
+    char *filename = argv[1];
+    int min = atoi(argv[2]);
+    int max = atoi(argv[3]);
+
+    FILE *out = fopen(filename, "w");
+    if (out == NULL) {
+        fprintf(stderr, "Cannot open %s\n", filename);
+    }
+
+    for (int i = min; i <= max; i++) {
+        fprintf(out, "%d\n", i);
+    }
+
+    return 0;
+}
