60784a5b77b0bee9981573e4731b7b72f2ce548e
Soorria Saruva <soorria.ss@gmail.com>
2020-07-24 10:04:26 +1000

    Complete print_bytes

--- lab08/print_bytes.c
+++ lab08/print_bytes.c
@@ -1,5 +1,30 @@
+// print_bytes.c
 
+#include <stdio.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s FILENAME\n", argv[0]);
+        return 1;
+    }
+
+    char *filename = argv[1];
+    FILE *in = fopen(filename, "r");
+    if (in == NULL) {
+        perror(filename);
+    }
+
+    char c;
+    int nRead = 0;
+    while ((c = fgetc(in)) != EOF) {
+        printf("byte %4d: %3d 0x%02x", nRead, c, c);
+        if (isprint(c)) {
+            printf(" '%c'", c);
+        }
+        printf("\n");
+        nRead++;
+    }
+
     return 0;
 }
