28d799c420f4a3bc370143cb4e5456d7924b047b
Soorria Saruva <soorria.ss@gmail.com>
2020-07-24 10:31:12 +1000

    Complete hidden_strings

--- lab08/hidden_strings.c
+++ lab08/hidden_strings.c
@@ -1,4 +1,45 @@
+// hidden_strings.c
+
+#include <stdio.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s FILENAME\n", argv[0]);
+        return 1;
+    }
+
+    char *filename = argv[1];
+    FILE *in = fopen(filename, "r");
+    if (in == NULL) {
+        perror(filename);
+        return 1;
+    }
+
+    int buffer[3] = {0};
+    int bufferIndex = 0;
+    int isPrinting = 0;
+    int c;
+
+    while ((c = fgetc(in)) != EOF) {
+        if (isprint(c)) {
+            if (bufferIndex < 3 && !isPrinting) {
+                buffer[bufferIndex] = c;
+                bufferIndex++;
+            } else {
+                if (bufferIndex) {
+                    for (int i = 0; i < 3; i++) {
+                        putchar(buffer[i]);
+                    }
+                    bufferIndex = 0;
+                }
+                putchar(c);
+                isPrinting = 1;
+            }
+        } else {
+            putchar('\n');
+        }
+    }
+
     return 0;
 }
