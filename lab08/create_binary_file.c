ef57eac84fdf8c87ece873aaa7d0bfd323afd81a
Soorria Saruva <soorria.ss@gmail.com>
2020-07-24 10:14:09 +1000

    Complete create_binary_file

--- lab08/create_binary_file.c
+++ lab08/create_binary_file.c
@@ -1,5 +1,25 @@
+// create_binary_file.c
 
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    return 0;
+    if (argc < 3) {
+        fprintf(stderr, "usage: %s FILENAME BYTES\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    char *filename = argv[1];
+
+    FILE *out = fopen(filename, "w");
+    if (out == NULL) {
+        perror(filename);
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 2; i < argc; i++) {
+        fputc(atoi(argv[i]), out);
+    }
+
+    return EXIT_SUCCESS;
 }
