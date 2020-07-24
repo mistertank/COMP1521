5b3a11000a11a85c441d19174583e6c8703f9002
Soorria Saruva <soorria.ss@gmail.com>
2020-07-24 11:16:27 +1000

    Complete last_line.c

--- lab08/last_line.c
+++ lab08/last_line.c
@@ -1,5 +1,37 @@
+// last_line.c
 
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s FILENAME\n", argv[0]);
+        return 1;
+    }
+
+    char *filename = argv[1];
+    FILE *in = fopen(filename, "r");
+    if (in == NULL) {
+        perror(filename);
+        return 1;
+    }
+
+    fseek(in, -2, SEEK_END);
+    int c = fgetc(in);
+
+    while (c != EOF && c != '\n') {
+        printf("c=%c %x\n", c, c);
+        fseek(in, -2, SEEK_CUR);
+        c = fgetc(in);
+    }
+
+    c = fgetc(in);
+    printf("c=%c %02x\n", c, c);
+
+    while (c != EOF && c != '\n') {
+        putchar(c);
+        c = fgetc(in);
+    }
+    putchar('\n');
+
     return 0;
 }
