d5c305b3fe62697fa58f684fdc6ff777f45e5e45
Soorria Saruva <soorria.ss@gmail.com>
2020-07-31 10:29:32 +1000

    try addi

--- lab09/addi.c
+++ lab09/addi.c
@@ -11,7 +11,13 @@
 
 // return the MIPS opcode for addi $t,$s, i
 uint32_t addi(int t, int s, int i) {
-
-    return 42; // REPLACE WITH YOUR CODE
-
+    uint32_t result = 0;
+    uint32_t tBits, sBits, iBits;
+    tBits = (t & 0x1F) << 16;
+    result += tBits;
+    sBits = (s & 0x1F) << 21;
+    result += sBits;
+    iBits = ((uint16_t)i) & 0xFFFF;
+    result += iBits;
+    return result;
 }
