570ec427a91f5a85552d559d8f10c411ef49fea1
Soorria Saruva <soorria.ss@gmail.com>
2020-07-31 10:43:32 +1000

    Try file_sizes

---
+++ lab09/file_sizes.c
@@ -0,0 +1,19 @@
+// file_sizes.c
+//
+// By Soorriamoorthy Saruva (z5258068)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[]) {
+    long int total = 0;
+    for (int i = 1; i < argc; i++) {
+        struct stat s;
+        stat(argv[i], &s);
+        total += (long)s.st_size;
+        printf("%s: %ld bytes\n", argv[i], (long)s.st_size);
+    }
+    return EXIT_SUCCESS;
+}
