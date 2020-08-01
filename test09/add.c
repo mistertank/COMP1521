a7a316dd9a3832942e06b735d082f7bd44fb8f3c
Soorria Saruva <soorria.ss@gmail.com>
2020-08-01 10:55:50 +1000

    Try add

--- test09/add.c
+++ test09/add.c
@@ -6,7 +6,9 @@
 
 // return the MIPS opcode for add $d, $s, $t
 uint32_t add(uint32_t d, uint32_t s, uint32_t t) {
-
-    return 42; // REPLACE WITH YOUR CODE
-
+    uint32_t result = 0x20;
+    result += (s & 0x1F) << 21;
+    result += (t & 0x1F) << 16;
+    result += (s & 0x1F) << 11;
+    return result;
 }
