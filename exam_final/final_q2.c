6d04570878d50b76098da461945dc9d18ad6ecf2
Soorria Saruva <soorria.ss@gmail.com>
2020-08-19 13:11:10 +1000

    Complete q2

--- exam_final/final_q2.c
+++ exam_final/final_q2.c
@@ -1,13 +1,15 @@
 #include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 // given a uint32_t value,
 // return 1 iff the least significant (bottom) byte
 // is equal to the 2nd least significant byte; and
 // return 0 otherwise
 int final_q2(uint32_t value) {
-    // PUT YOUR CODE HERE
-
-    return 42;
+    uint32_t lowestByte = value & 0xFF;
+    uint32_t secondLowestByte = (value & 0xFF00) >> 8;
+    printf("%x %x\n", lowestByte, secondLowestByte);
+    return lowestByte == secondLowestByte;
 }
